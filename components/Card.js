ce5957e42c49e91655976f4ef8c22ad29d222396
Allan Gadelha <allangadelha@gmail.com>
2022-06-08 19:26:31 -0300

    Finishing CSS to page individual profile

--- components/Card.js
+++ components/Card.js
@@ -1,11 +1,30 @@
 import Link from 'next/link';
+import { FaFacebook, FaInstagram, FaLinkedin, FaGithub } from 'react-icons/fa';
+
 import styles from '../styles/Card.module.css';
+
+export function iconSocialNetwork(social_network){
+    if(social_network == 'Facebook'){
+        return <FaFacebook />
+    } else if(social_network == 'Instagram'){
+        return <FaInstagram />
+
+    } else if(social_network == 'LinkedIn'){
+        return <FaLinkedin />
+
+    } else if(social_network == 'Github'){
+        return <FaGithub />
+
+    }
+}
 
 export default function Card({profile}) {
 
     return (
         <div className={styles.card}>
             <h3 className={styles.title}>{profile.social_network}</h3>
+            <h3 className={`${styles[profile.social_network]}`}>
+                {iconSocialNetwork(profile.social_network)}</h3>
             <Link href={`/profile/${profile.id}`}>
                 <a className={styles.btn}>Detalhes</a>
             </Link>
--- pages/profile/[profileId].js
+++ pages/profile/[profileId].js
@@ -1,16 +1,20 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa';
+
+import styles from '../../styles/Profile.module.css';
+
 export async function getStaticPaths() {
 
     const api = 'http://localhost:4000/profiles';
-  
+
     const res = await fetch(`${api}`);
     const data = await res.json();
 
-    console.log(data)
-  
     const paths = data.map((profile, index) => {
-        
+
         return {
-            params: { profileId: (index + 1).toString()}
+            params: { profileId: (index + 1).toString() }
         }
     })
 
@@ -18,34 +22,51 @@ export async function getStaticPaths() {
         paths,
         fallback: false
     }
-    
+
 }
 
-export async function getStaticProps(context) {
 
+export async function getStaticProps(context) {
+    
     const id = context.params.profileId;
-  
+    
     const api = 'http://localhost:4000/profiles';
     const res = await fetch(`${api}/${id}`);
     const data = await res.json();
-  
+    
     return {
-      props: {
-        profile: data
-      }
+        props: {
+            profile: data
+        }
     }
-  
-  }
+    
+}
 
-
-export default function Profile({profile}) {
+export default function Profile({ profile }) {
 
     return (
-        <>
-            <p>Nome: {profile.name}</p>
+        <div className={styles.profile_container}>
+            <h1 className={styles.title}>{profile.name}</h1>
             <p>Rede Social: {profile.social_network}</p>
-            <p>Link de Acesso: {profile.link}</p>
-        </>
+            <p className={styles.image}>
+                <Link href={profile.link}>
+                    <a>
+                        <Image
+                            src={profile.image_link}
+                            width="100"
+                            height="100"
+                            alt={profile.social_network}
+                        />
+                    </a>
+                </Link>
+            </p>
+            <p><Link href={profile.link}><a>Clique aqui para acessar</a></Link></p>
+            <div className={styles.data_container}>
+                <Link href="/">
+                    <a className={styles.btn}>Voltar ao início</a>
+                </Link>
+            </div>
+        </div>
     )
 
 }
