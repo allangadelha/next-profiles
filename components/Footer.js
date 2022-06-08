501d498184e412e9001bc954c385ee2a3f4c556a
Allan Gadelha <allangadelha@gmail.com>
2022-06-08 15:21:24 -0300

    CSS in Footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,9 @@
+import styles from '../styles/Footer.module.css';
+
 export default function Footer() {
 
     return (
-        <footer>
+        <footer className={styles.footer}>
             <p>
                 <span>Next Profiles</span> | &copy; { new Date().getFullYear() }
             </p>
