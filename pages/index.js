6d36ab5c74194a44c2b485f198718fc87599c37c
Allan Gadelha <allangadelha@gmail.com>
2022-06-08 16:14:54 -0300

    Getting data from API

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,31 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export async function getStaticProps() {
+
+  const api = 'http://localhost:4000/profiles';
+
+  const res = await fetch(`${api}`);
+  const data = await res.json();
+
+  console.log(data);
+
+  return {
+    props: {
+      profiles: data
+    }
+  }
+
+}
+
+export default function Home({profiles}) {
   return (
     <div className={styles.container}>
-      
+      <h1>Perfis</h1>
+      <ul>
+        {profiles.map((profile) => (
+          <li key={profile.id}>{profile.id}</li>
+        ))}
+      </ul>
     </div>
   )
 }
